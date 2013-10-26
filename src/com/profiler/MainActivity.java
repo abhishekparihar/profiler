f3978a232086150092ea1dc0d3f9c4c5888eab91
Abhishek Parihar <abhishek@weboniselab.com>
2013-10-26 17:30:59 +0530

    new UI for main.xml

--- src/com/profiler/MainActivity.java
+++ src/com/profiler/MainActivity.java
@@ -1,23 +1,65 @@
 package com.profiler;
 
-import android.os.Bundle;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.app.WallpaperManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private FileInputStream is;
+	private BufferedInputStream bis;
+	private WallpaperManager wallpaperManager;
+	private Drawable wallpaperDrawable;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+
+	public void onChangeClicked(View v){
+		File sdcard = Environment.getExternalStorageDirectory();  
+		try {
+			is = new FileInputStream(new File(sdcard,"wall.jpg"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		bis = new BufferedInputStream(is);
+		Bitmap bitmap = BitmapFactory.decodeStream(bis);
+		Bitmap useThisBitmap = Bitmap.createBitmap(bitmap);
+		//bitmap.recycle();
+		//if(imagePath!=null){
+		System.out.println("Hi I am try to open Bit map");
+		wallpaperManager = WallpaperManager.getInstance(this);
+		wallpaperDrawable = wallpaperManager.getDrawable();
+		try {
+			wallpaperManager.setBitmap(useThisBitmap);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//	}
+	}
+
 }
