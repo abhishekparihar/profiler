5985993c3d734d15a6c2d59e53bb783c877169e7
ranaRanvijay <rana.ranvijay@weboniselab.com>
2013-10-26 18:08:03 +0530

    galarry work complete

--- src/com/profiler/MainActivity.java
+++ src/com/profiler/MainActivity.java
@@ -1,32 +1,34 @@
 package com.profiler;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-	
+
 	final static String TAG = "MainActivity";
+	Button createProfileButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
+		createProfileButton = (Button) findViewById(R.id.createProfile);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+		createProfileButton.setOnClickListener(new OnClickListener() {
 
-	public void onCreateProfileClicked(View v) {
-		Log.i(TAG, "onCreateProfileClicked");
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(MainActivity.this,
+						CreateProfileActivity.class);
+				startActivity(intent);
+			}
+		});
 	}
-    
+
 }
